fd3155c5f65c620f6278ee0cd0912bff013df8fe
Gabriel Marquez <gabriel@marquezgabriel.com>
2022-05-16 13:50:02 -0400

    Initial Commit

---
+++ Recipe-List-App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe-List-App
+//
+//  Created by Gabriel Marquez on 2022-05-16.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
